14e83bb87cc079cb69668ad98c7f0e9642ee4736
SalomeNderu <sallienderu@gmail.com>
2023-02-28 12:15:45 +0300

    added models to my files

---
+++ django_registration_nderuh/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+class Student(models.Model):
+    name = models.CharField(max_length=50, blank=True, null=False)
+    email = models.EmailField(max_length=50, blank=True, null=False)
+    age = models.IntegerField(max_length=50, blank=True, null=False)
+    gender = models.CharField(max_length=50, blank=True, null=False)
+def __str__(self):
